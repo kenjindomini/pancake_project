e6619ef1348b45658c1cfc595d218b54197b26d5
Keith Olenchak <keith.olenchak@gmail.com>
2015-10-11 10:51:47 -0400

    linted

--- src/main.jsx
+++ src/main.jsx
@@ -15,40 +15,40 @@ let App = React.createClass({
       sortColumn: 'name',
       sortDirection: 'az',
       sorted: [],
-      pocTest: "",
-      note: "",
+      pocTest: '',
+      note: '',
       userCount: 0
     };
   },
   componentDidMount () {
-    //POC Test for usersByName.set, This one does not display anything except the console.
-    //changed Aimee to enabled and changed her office to portland, aida was changed to enabled.
-    console.log("seting user info via usersByName.");
+    // POC Test for usersByName.set, This one does not display anything except the console.
+    // changed Aimee to enabled and changed her office to portland, aida was changed to enabled.
+    console.log('seting user info via usersByName.');
     var setUsersByName = {
       json: {
         usersByName: {
-          "Aimee Farmer": {
-            "is_enabled": true,
-            "name": "Aimee Farmer",
-            "company": "TELLIFLY",
-            "email": "aimeefarmer@tellifly.com",
-            "phone": "(823) 547-2743",
-            "office": "Portland"
+          'Aimee Farmer': {
+            is_enabled: true,
+            name: 'Aimee Farmer',
+            company: 'TELLIFLY',
+            email: 'aimeefarmer@tellifly.com',
+            phone: '(823) 547-2743',
+            office: 'Portland'
           },
-          "Aida Vega": {
-            "is_enabled": true,
-            "name": "Aida Vega",
-            "company": "DIGINETIC",
-            "email": "aidavega@diginetic.com",
-            "phone": "(875) 559-2174",
-            "office": "NYC"
+          'Aida Vega': {
+            is_enabled: true,
+            name: 'Aida Vega',
+            company: 'DIGINETIC',
+            email: 'aidavega@diginetic.com',
+            phone: '(875) 559-2174',
+            office: 'NYC'
           }
         }
       }
     };
     falcorModel.set(setUsersByName).then((data) => {
       console.log(data);
-      //POC Test for usersAscendingSort.
+      // POC Test for usersAscendingSort.
       this.getUsersAscending();
     });
   },
@@ -74,29 +74,29 @@ let App = React.createClass({
 
           <p>Select POC test:
             &nbsp;&nbsp;<input
-                type="button"
-                name="Users-Ascending Sort"
-                value="Get Users(ASC)"
+                type='button'
+                name='Users-Ascending Sort'
+                value='Get Users(ASC)'
                 onClick={this.getUsersAscending}/>
             &nbsp;&nbsp;<input
-                type="button"
-                name="Users-Descending Sort"
-                value="Get Users(DESC)"
+                type='button'
+                name='Users-Descending Sort'
+                value='Get Users(DESC)'
                 onClick={this.getUsersDescending}/>
             &nbsp;&nbsp;<input
-                type="button"
-                name="Users-Unsorted"
-                value="All users"
+                type='button'
+                name='Users-Unsorted'
+                value='All users'
                 onClick={this.getUsers}/>
             &nbsp;&nbsp;<input
-                type="button"
-                name="OfficesByName"
-                value="Get Offices"
+                type='button'
+                name='OfficesByName'
+                value='Get Offices'
                 onClick={this.getOffices}/>
             &nbsp;&nbsp;<input
-                type="button"
-                name="Total Users"
-                value="Get Total users"
+                type='button'
+                name='Total Users'
+                value='Get Total users'
                 onClick={this.getUserCount}/>
           </p>
 
@@ -113,25 +113,25 @@ let App = React.createClass({
         </div>
     );
   },
-  getUsersAscending() {
-    //POC test for usersAscendingSort
-    console.log("loading sorted (Ascending) user info.");
+  getUsersAscending () {
+    // POC test for usersAscendingSort
+    console.log('loading sorted (Ascending) user info.');
     falcorModel.get('usersAscendingSort[2..15]["name","email","is_enabled","company","office","uid"]').then((data) => {
       console.log(data);
       this.setState({
-        sorted: toArry(data.json.usersAscendingSort), pocTest: "Testing usersAscendingSort"
+        sorted: toArry(data.json.usersAscendingSort), pocTest: 'Testing usersAscendingSort'
       });
       this.setState({
         userCount: this.state.sorted.length
       });
     });
   },
-  getUsersDescending() {
-    //POC test for usersDescendingSort
-    console.log("loading sorted(Descending) user info.");
+  getUsersDescending () {
+    // POC test for usersDescendingSort
+    console.log('loading sorted(Descending) user info.');
     falcorModel.get('usersDescendingSort[0..15]["name","email","is_enabled","company","office","uid"]').then((data) => {
       this.setState({
-        sorted: toArry(data.json.usersDescendingSort), sortDirection: 'za', pocTest: "Testing usersDescendingSort"
+        sorted: toArry(data.json.usersDescendingSort), sortDirection: 'za', pocTest: 'Testing usersDescendingSort'
       });
       this.setState({
         userCount: this.state.sorted.length
@@ -139,20 +139,20 @@ let App = React.createClass({
       console.log(data);
     });
   },
-  getUsers() {
-    //POC test for unsorted users call and users.length.
-    console.log("getting total number of users.");
+  getUsers () {
+    // POC test for unsorted users call and users.length.
+    console.log('getting total number of users.');
     falcorModel.get('users.length').then((data) => {
       console.log(data);
       var numberOfUsers = data.json.users.length - 1;
-      console.log("Loading unsorted user list.");
+      console.log('Loading unsorted user list.');
       if (numberOfUsers === undefined) {
         numberOfUsers = 19;
       }
       falcorModel.get('users[0..' + numberOfUsers + ']["name","email","is_enabled","company","office","uid"]').then((d) => {
         console.log(d);
         this.setState({
-          sorted: sortBy(d.json.users, this.state.sortColumn), pocTest: "Testing users.length"
+          sorted: sortBy(d.json.users, this.state.sortColumn), pocTest: 'Testing users.length'
         });
         this.setState({
           userCount: this.state.sorted.length
@@ -161,25 +161,25 @@ let App = React.createClass({
       });
     });
   },
-  getUserCount() {
-    //POC test users.length
-    console.log("getting total number of users.");
+  getUserCount () {
+    // POC test users.length
+    console.log('getting total number of users.');
     falcorModel.get('users.length').then((data) => {
       console.log(data);
       var numberOfUsers = data.json.users.length;
       this.setState({
         userCount: numberOfUsers
       });
-  });
+    });
   },
-  getOffices() {
-    //POC Test for offices. This one does not display due to the way the table is dynamically created.
-    console.log("loading office info.");
+  getOffices () {
+    // POC Test for offices. This one does not display due to the way the table is dynamically created.
+    console.log('loading office info.');
     falcorModel.get('offices["Maynard", "NYC"]["address"]').then((data) => {
       this.setState({
-        sorted: toArry(data.json.offices), pocTest: "Testing offices", note: JSON.stringify(data.json.offices, null, 2)
+        sorted: toArry(data.json.offices), pocTest: 'Testing offices', note: JSON.stringify(data.json.offices, null, 2)
       });
-      console.log(data.json.offices)
+      console.log(data.json.offices);
     });
   }
 });
