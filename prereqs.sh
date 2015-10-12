ac0019b0b12fd0393177af585d3ec3d84f16ddb3
Keith Olenchak <keith.olenchak@gmail.com>
2015-10-12 14:20:44 -0400

    setting source, installing node 4.1.1 and setting it as the default.

--- prereqs.sh
+++ prereqs.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get install -y build-essential libssl-dev git
-sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | NVM_DIR=/usr/local/nvm bash
+sudo apt-get install -y build-essential libssl-dev git python2.7
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+source /home/vagrant/.profile
+nvm install 4.1.1
+nvm alias default 4.1.1
