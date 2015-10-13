4a882698f05e51c6016553485ccd26c2c73fca36
Keith Olenchak <keith.olenchak@gmail.com>
2015-10-13 08:11:14 -0400

    replaced /home/vagrant with ~ in hopes the scripts will still be useful even if vagrant cannot be made to work in time.

--- prereqs.sh
+++ prereqs.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev git python2.7
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
-source /home/vagrant/.profile
+echo "source ~/.nvm/nvm.sh" >> ~/.profile
+source ~/.profile
 nvm install 4.1.1
 nvm alias default 4.1.1
